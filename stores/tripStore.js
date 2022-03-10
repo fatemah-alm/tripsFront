8d1e98c7b0926ced0abb873f95f9e7d8029cfca2
fahad alajmi <fahadalajmi126@gmail.com>
2022-03-10 15:35:42 +0300

    trip delete

--- stores/tripStore.js
+++ stores/tripStore.js
@@ -1,10 +1,20 @@
 import { makeAutoObservable } from "mobx";
+import { instance } from "./instance";
 
 class TripStore {
+  trips = [];
   constructor() {
     makeAutoObservable(this);
   }
-  trips = [];
+
+  deleteTrip = (id) => {
+    try {
+      this.trips = this.trips.filter((trip) => trip._id !== id);
+      await instance.delete(`/api/trips/${id}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
 
 const tripStore = new TripStore();
