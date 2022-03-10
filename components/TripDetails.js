4b24ccba13d5034c792cf79272d79720c9110657
fahad alajmi <fahadalajmi126@gmail.com>
2022-03-10 15:38:35 +0300

    trip delete

--- components/TripDetails.js
+++ components/TripDetails.js
@@ -1,10 +1,22 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Button } from "native-base";
+import tripStore from "../stores/tripStore";
 
 const TripDetails = () => {
   return (
     <SafeAreaView>
       <Text>TripDetails</Text>
+      <Button
+        m={3}
+        colorScheme="amber"
+        variant="outline"
+        onPress={() => tripStore.deleteTrip()}
+        borderRadius={30}
+        flex={1}
+      >
+        Delete
+      </Button>
     </SafeAreaView>
   );
 };
